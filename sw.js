b050a58a4ac5d6ab42c82c013a6ece81b39e3130
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-02-12 13:00:45 +0000

    Cache Versioning

--- sw.js
+++ sw.js
@@ -1,29 +1,39 @@
-const staticCacheName = 'site-static';
+const staticCacheName = "site-static-v1";
 const assets = [
-  '/',
-  '/index.html',
-  '/js/app.js',
-  '/js/ui.js',
-  'js/materialize.min.js',
-  'css/styles.css',
-  'css/materialize.min.css',
-  'img/dish.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "js/materialize.min.js",
+  "css/styles.css",
+  "css/materialize.min.css",
+  "img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
 ];
 // install service worker
 self.addEventListener("install", evt => {
   // console.log("service worker has been installed.");
   evt.waitUntil(
     caches.open(staticCacheName).then(cache => {
-      cache.addAll(assets)
+      cache.addAll(assets);
     })
-  )
+  );
 });
 
 // activate service worker
 self.addEventListener("activate", evt => {
   //console.log("service worker has been activated");
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 // fetch event
@@ -33,5 +43,5 @@ self.addEventListener("fetch", evt => {
     caches.match(evt.request).then(cacheRes => {
       return cacheRes || fetch(evt.request);
     })
-  )
+  );
 });
