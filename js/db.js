17f6ecb9bf91e9d87478b75c7e605ad79d0ce923
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-02-12 16:25:59 +0000

    Updating the UI

---
+++ js/db.js
@@ -0,0 +1,12 @@
+//real time listener
+db.collection("recipes").onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    if (change.type === "added") {
+      //add the document data to the web page
+      renderRecipe(change.doc.data(), change.doc.id);
+    }
+    if (change.type === "removed") {
+      //remove the document data from the web page
+    }
+  });
+});
